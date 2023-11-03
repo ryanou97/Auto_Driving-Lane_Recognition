5f12188ee8a474ca3b4e30759254b819efff5cd7
ryanou97 <zhonglinou.1997@gmail.com>
2023-11-03 18:06:51 +0800

    first commit

---
+++ auto.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+@author: ryanou97
+"""
+
+import cv2
+
+img = cv2.imread('C:\\Users\\smile\\.spyder-py3\\Project_Auto-Driving_Lane_Recognition\\road.jpg')    # 讀取圖片
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)    # 灰階處理
+blur = cv2.GaussianBlur(gray, (3, 3), 0)        # 高斯模糊
+cv2.imshow('Normal', img)   # 顯示原始圖片
+cv2.imshow('Gray', gray)    # 顯示灰階圖片
+cv2.imshow('Blur', blur)    # 顯示高斯模糊圖片
+cv2.waitKey(0)
+cv2.destroyAllWindows()
